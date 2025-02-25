a147110337098c2d2d0f79034878c9844e22cdaa
Dasse <pascaldasse56@gmail.com>
2025-02-25 10:52:29 +0100

    add login reset

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Volt::route('/', 'index');
 Route::middleware('guest')->group(function () {
 	Volt::route('/register', 'auth.register');
     Volt::route('/login', 'auth.login')->name('login');
+    Volt::route('/forgot-password', 'auth.forgot-password');
+    Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
 });
 
 Route::middleware('auth')->group(function () {
