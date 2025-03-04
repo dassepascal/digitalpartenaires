363c1c92235fde9bef6c503115312fd38a7a062f
Dasse <pascaldasse56@gmail.com>
2025-03-04 15:19:59 +0100

    modif apparence e-commerce

--- resources/views/livewire/services/e-commerce.blade.php
+++ resources/views/livewire/services/e-commerce.blade.php
@@ -7,100 +7,91 @@ new class extends Component {
 }; ?>
 
 <div style="max-width: 700px; margin: 0 auto;">
-    <x-card title="Boostez vos ventes avec l'e-commerce: Le guide ultime pour les PME">
+    <x-card title="Boostez vos ventes avec l'e-commerce: Le guide ultime pour les PME" shadow separator>
         <p>Que vous dirigiez une PME ou envisagiez de lancer votre entreprise, l'e-commerce est devenu une nécessité
             plutôt qu'une option. Avec l'énorme potentiel que recèle Internet, augmenter vos ventes n'a jamais été aussi
             facile. Comprendre comment maximiser ces opportunités peut être le facteur qui fera évoluer votre
             entreprise. Voyons comment.</p>
-
+    </x-card>
+    <x-card title="1: Pourquoi l'E-Commerce est-il un levier de croissance pour votre PME?">
+       
         <x-slot:figure>
             <img src="https://picsum.photos/500/200" />
         </x-slot:figure>
+    
+    <ol>
+        
+        <li>
+            <h2 class=" font-bold mb-2">1.1: Hausse de la demande en ligne.</h2>
+            <p class="mb-4">Sherpa Marketing a constaté que les achats en ligne ont augmenté de 69% depuis 2020. Les PME ont donc
+                une occasion unique de tirer profit de cette tendance croissante. C'est une opportunité de répondre
+                à la demande en ligne, de gagner de nouveaux clients et finalement, d'augmenter les ventes.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">1.2: Extension du marché à un public international</h2>
+            <p class="mb-4"> Que votre objectif soit de vendre au niveau local ou mondial, l'Internet est un marché sans
+                frontières. Vous pouvez atteindre des clients à l'autre bout du monde sans franchir votre porte. Cette
+                exposition prolongée peut aider vos ventes à grimper en flèche.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">1.3: Disponibilité 24/7</h2>
+            <p class="mb-4">Contrairement à un magasin physique, l'e-commerce fonctionne 24 heures sur 24, 7 jours sur 7, offrant
+                aux clients la possibilité de faire des achats sans contrainte de temps. Cette accessibilité sans
+                précédent peut stimuler vos ventes comme jamais auparavant.</p>
+        </li>
+    </ol>
 
-        <ol>
-            <li>
-                <h2 class="text-2xl font-bold my-4">1: Pourquoi l'E-Commerce est-il un levier de croissance pour votre
-                    PME?</h2>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">1.1: Hausse de la demande en ligne.</h2>
-                <p>Sherpa Marketing a constaté que les achats en ligne ont augmenté de 69% depuis 2020. Les PME ont donc
-                    une occasion unique de tirer profit de cette tendance croissante. C'est une opportunité de répondre
-                    à la
-                    demande en ligne, de gagner de nouveaux clients et finalement, d'augmenter les ventes.</p>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">1.2: Extension du marché à un public international</h2>
-                <p>Que votre objectif soit de vendre au niveau local ou mondial, l'Internet est un marché sans
-                    frontières.
-                    Vous pouvez atteindre des clients à l'autre bout du monde sans franchir votre porte. Cette
-                    exposition
-                    prolongée peut aider vos ventes à grimper en flèche.</p>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">1.3: Disponibilité 24/7</h2>
-                <p>Contrairement à un magasin physique, l'e-commerce fonctionne 24 heures sur 24, 7 jours sur 7, offrant
-                    aux clients la possibilité de faire des achats sans contrainte de temps. Cette accessibilité sans
-                    précédent peut stimuler vos ventes comme jamais auparavant.</p>
-            </li>
-        </ol>
+    <ol>
+        <li>
+            <h2 class="text-2xl font-bold my-4">2: Comment optimiser votre stratégie e-commerce pour maximiser les ventes?</h2>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">2.1 SEO: Le pilier de la visibilité en ligne</h2>
+            <p class="mb-4">Le SEO (Search Engine Optimization) est une méthode efficace pour augmenter votre visibilité en
+                ligne. En optimisant le contenu de votre site pour les moteurs de recherche tels que Google, vous pouvez
+                atteindre les premières positions, attirer plus de trafic et donc augmenter les ventes.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">2.2 Utilisation du marketing digital pour promouvoir votre e-commerce</h2>
+            <p class="mb-4">Le marketing digital joue un rôle crucial dans la visibilité et la réputation de votre e-commerce. De
+                l'utilisation des médias sociaux, aux campagnes d'e-mailing, en passant par le marketing de contenu,
+                ces outils peuvent augmenter votre visibilité et attirer plus de clients vers votre site.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">2.3 Proposer une expérience d'achat optimale</h2>
+            <p class="mb-4">Un utilisateur satisfait est un client fidèle. L'optimisation du processus d'achat, de la navigation
+                sur le site à la passation de commande, peut améliorer l'expérience client et augmenter les chances de
+                fidélisation et de recommandations.</p>
+        </li>
+    </ol>
+    <ol>
+        <li>
+            <h2 class="text-2xl font-bold mb-2 ">3: Études de cas : PME ayant réussi grâce à l'e-commerce</h2>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">3.1 Entreprise 1 : Boost des ventes grâce à l'expansion internationale</h2>
+            <p class="mb-4">D'abord une petite entreprise locale, l'entreprise 1 a connu une croissance exponentielle après son
+                expansion internationale grâce à l'e-commerce. Leurs ventes ont augmenté de 150% en seulement 6 mois.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2 ">3.2 Entreprise 2: Stratégie SEO réussie</h2>
+            <p class="mb-4">L'entreprise 2 a doublé son trafic organique en seulement 3 mois grâce à une stratégie de référencement
+                bien planifiée. Grâce à cette nouvelle visibilité, ses ventes ont augmenté de 90%.</p>
+        </li>
+        <li>
+            <h2 class=" font-bold mb-2">3.3 Entreprise 3: Amélioration de l'expérience utilisateur</h2>
+            <p class="mb-4"> Avec une refonte majeure de leur site pour une expérience utilisateur plus fluide, l'entreprise 3 a vu
+                ses ventes augmenter de 60%.</p>
+        </li>
+    </ol>
 
-        <ol>
-            <li>
-                <h2 class="text-2xl font-bold my-4">2: Comment optimiser votre stratégie e-commerce pour maximiser les
-                    ventes?</h2>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">2.1 SEO: Le pilier de la visibilité en ligne</h2>
-                <p>Le SEO (Search Engine Optimization) est une méthode efficace pour augmenter votre visibilité en
-                    ligne.
-                    En optimisant le contenu de votre site pour les moteurs de recherche tels que Google, vous pouvez
-                    atteindre les premières positions, attirer plus de trafic et donc augmenter les ventes.</p>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">2.2 Utilisation du marketing digital pour promouvoir votre e-commerce
-                </h2>
-                <p>Le marketing digital joue un rôle crucial dans la visibilité et la réputation de votre e-commerce. De
-                    l'utilisation des médias sociaux, aux campagnes d'e-mailing, en passant par le marketing de contenu,
-                    ces
-                    outils peuvent augmenter votre visibilité et attirer plus de clients vers votre site.</p>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">2.3 Proposer une expérience d'achat optimale</h2>
-                <p>Un utilisateur satisfait est un client fidèle. L'optimisation du processus d'achat, de la navigation
-                    sur
-                    le site à la passation de commande, peut améliorer l'expérience client et augmenter les chances de
-                    fidélisation et de recommandations.</p>
-            </li>
-        </ol>
-        <ol>
-            <li>
-                <h2 class="text-2xl font-bold my-4">3: Études de cas : PME ayant réussi grâce à l'e-commerce</h2>
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">3.1 Entreprise 1 : Boost des ventes grâce à l'expansion
-                    internationale</h2>
-                D'abord une petite entreprise locale, l'entreprise 1 a connu une croissance exponentielle après son
-                expansion internationale grâce à l'e-commerce. Leurs ventes ont augmenté de 150% en seulement 6 mois.
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">3.2 Entreprise 2: Stratégie SEO réussie</h2>
-                L'entreprise 2 a doublé son trafic organique en seulement 3 mois grâce à une stratégie de référencement
-                bien planifiée. Grâce à cette nouvelle visibilité, ses ventes ont augmenté de 90%.
-            </li>
-            <li>
-                <h2 class="text-2xl font-bold my-4">3.3 Entreprise 3: Amélioration de l'expérience utilisateur</h2>
-                Avec une refonte majeure de leur site pour une expérience utilisateur plus fluide, l'entreprise 3 a vu
-                ses ventes augmenter de 60%.
-            </li>
-        </ol>
-
-        <h2 class="text-2xl font-bold my-4">Conclusion</h2>
-        <p>Un marché en expansion constante, une visibilité mondiale et un fonctionnement 24/7 faits de l'e-commerce un
-            puissant levier de croissance pour les PME. En optimisant le référencement, en utilisant efficacement le
-            marketing digital et en proposant une excellente expérience utilisateur, les PME peuvent dynamiser leur
-            croissance comme jamais auparavant. Alors n'attendez plus, exploitez le potentiel de l'e-commerce dès
-            aujourd'hui et regardez vos ventes s'envoler!</p>
+    <h2 class="text-2xl font-bold my-4">Conclusion</h2>
+    <p>Un marché en expansion constante, une visibilité mondiale et un fonctionnement 24/7 faits de l'e-commerce un
+        puissant levier de croissance pour les PME. En optimisant le référencement, en utilisant efficacement le
+        marketing digital et en proposant une excellente expérience utilisateur, les PME peuvent dynamiser leur
+        croissance comme jamais auparavant. Alors n'attendez plus, exploitez le potentiel de l'e-commerce dès
+        aujourd'hui et regardez vos ventes s'envoler!</p>
+        <span class="text-2xl font-bold my-4 text-red-500"> Prendre rdv</span>
     </x-card>
-   <span class="text-2xl font-bold my-4 text-red-500"> Prendre rdv</span>
+   
 </div>
