ca27b61f8ace7784b0f83034e84619737a4faa56
Dasse <pascaldasse56@gmail.com>
2025-03-11 21:53:01 +0100

    mofi index

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -6,9 +6,9 @@ new class extends Component {
     //
 }; ?>
 
-{{-- @if ()
+<!-- {{-- @if ()
 <x-header title="{{ __('Posts for category ') }} {{ $category->title }}" size="text-2xl sm:text-3xl md:text-4xl" />
-@endif --}}
+@endif --}} -->
 
 
 
@@ -24,21 +24,6 @@ new class extends Component {
     @endif
   
      
-    <x-card  title="Présentation de l'agence" >
-        
-     
-        <x-slot:figure>
-            <img src="{{ asset('storage/photos/pre2.jpg')}}" alt="pre2" />
-        </x-slot:figure>
-        <div class="text-justify">Digital Partenaire a été fondé en 2025 par Dasse Pascal, un passionné de digital et de technologies web, avec pour objectif d'aider les entreprises locales à se développer en ligne.</div>
-   
-   
-    </x-card>
-</div>
+    
 </div>
 
-
-
-</div>
-
-</div>
