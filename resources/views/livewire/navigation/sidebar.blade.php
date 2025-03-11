5e507f1370efccf923a3359a9649f1d026159473
Dasse <pascaldasse56@gmail.com>
2025-03-11 22:11:52 +0100

    modif sidebar

--- resources/views/livewire/navigation/sidebar.blade.php
+++ resources/views/livewire/navigation/sidebar.blade.php
@@ -3,37 +3,36 @@
 use Illuminate\Support\Facades\{Auth, Session};
 use Livewire\Volt\Component;
 
-new class() extends Component {
-
-	public function logout(): void
-	{
-		Auth::guard('web')->logout();
-		Session::invalidate();
-		Session::regenerateToken();
-		$this->redirect('/');
-	}
+new class extends Component {
+    public function logout(): void
+    {
+        Auth::guard('web')->logout();
+        Session::invalidate();
+        Session::regenerateToken();
+        $this->redirect('/');
+    }
 };
 ?>
 
 <div>
     <x-menu activate-by-route>
-        {{-- Utilisateur --}}
-        @if($user = auth()->user())
+        @if ($user = auth()->user())
+            <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover
+                class="-mx-2 !-my-2 rounded">
+                <x-slot:actions>
+                    <x-button icon="o-power" wire:click="logout" class="btn-circle btn-ghost btn-xs"
+                        tooltip-left="{{ __('Logout') }}" no-wire-navigate />
+                </x-slot:actions>
+            </x-list-item>
             <x-menu-separator />
-                <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                    <x-slot:actions>
-                        <x-button icon="o-power" wire:click="logout" class="btn-circle btn-ghost btn-xs" tooltip-left="{{ __('Logout') }}" no-wire-navigate />
-                    </x-slot:actions>
-                    <x-menu-separator />
             @if ($user->isAdmin())
                 <x-menu-item title="{{ __('Administration') }}" icon="s-building-office-2" link="{{ route('admin') }}" />
             @endif
             <x-menu-separator />
-                </x-list-item>
-                <x-menu-item title="{{ __('My profile') }}" icon="o-user" link="{{ route('profile') }}" />
-				<x-menu-item title="{{ __('My addresses') }}" icon="o-map-pin" link="{{ route('addresses') }}" />
-				<x-menu-item title="{{ __('My orders') }}" icon="o-shopping-cart" link="" />
-				<x-menu-item title="{{ __('RGPD') }}" icon="o-lock-closed" link="" />
+            <x-menu-item title="{{ __('My profile') }}" icon="o-user" link="{{ route('profile') }}" />
+            <x-menu-item title="{{ __('My addresses') }}" icon="o-map-pin" link="{{ route('addresses') }}" />
+           
+            <x-menu-item title="{{ __('RGPD') }}" icon="o-lock-closed" link="#" />
             <x-menu-separator />
         @else
             <x-menu-item title="{{ __('Login') }}" link="/login" />
