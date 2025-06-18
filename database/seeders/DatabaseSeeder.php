907e88e693d4005c014574a0d352a7b5ef9fb0ff
Dasse <pascaldasse56@gmail.com>
2025-06-18 17:23:21 +0200

    add menus+ submenu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,7 +40,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PostSeeder::class,
-               BlogPageSeeder::class,
+            BlogPageSeeder::class,
+            MenusSeeder::class,
         ]);
 
         $adminUser = new User();
