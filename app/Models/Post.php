1b9f35a6aa9fe053028615713e6d2c597d2c07a6
Dasse <pascaldasse56@gmail.com>
2025-06-18 17:46:37 +0200

    add comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,28 +17,29 @@ class Post extends Model
     protected $fillable = ['title', 'slug', 'body', 'active', 'image', 'user_id', 'category_id', 'seo_title', 'meta_description', 'meta_keywords', 'pinned'];
 
     public function user(): BelongsTo
-	{
-		return $this->belongsTo(User::class);
-	}
+    {
+        return $this->belongsTo(User::class);
+    }
 
-	public function category(): BelongsTo
-	{
-		return $this->belongsTo(Category::class);
-	}
-    public function comments(): HasMany
-	{
-		return $this->hasMany(Comment::class);
-	}
-
-	public function validComments(): HasMany
-	{
-		return $this->comments()->whereHas('user', function ($query) {
-			$query->whereValid(true);
-		});
-	}
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
 
     public function favoritedByUsers(): BelongsToMany
-{
-	return $this->belongsToMany(User::class, 'favorites');
-}
+    {
+        return $this->belongsToMany(User::class, 'favorites');
+    }
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function validComments(): HasMany
+    {
+        return $this->comments()->whereHas('user', function ($query) {
+            $query->whereValid(true);
+        });
+    }
 }
