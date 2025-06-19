aa8cd0a32f5207773f969bf14f6b2da1dec5de71
Dasse <pascaldasse56@gmail.com>
2025-06-19 20:04:56 +0200

    modif helpers + create helpersTest

--- app/Helpers/ContentHelpers.php
+++ app/Helpers/ContentHelpers.php
@@ -1,16 +1,10 @@
 <?php
 
 if (!function_exists('replaceAbsoluteUrlsWithRelative')) {
-    function replaceAbsoluteUrlsWithRelative(string $content)
+    function replaceAbsoluteUrlsWithRelative(string $html): string
     {
-        $baseUrl = url('/');
-        if ('/' !== substr($baseUrl, -1)) {
-            $baseUrl .= '/';
-        }
-
-        $pattern     = '/<img\s+[^>]*src="(?:https?:\/\/)?' . preg_quote(parse_url($baseUrl, PHP_URL_HOST), '/') . '\/([^"]+)"/i';
-        $replacement = '<img src="/$1"';
-
-        return preg_replace($pattern, $replacement, $content);
+        $baseUrl = rtrim(url('/'), '/') . '/';
+        // Remplacer toutes les occurrences de $baseUrl par "/"
+        return str_replace($baseUrl, '/', $html);
     }
 }
