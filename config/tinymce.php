33aa674f06a82ac6825cb24cd0a233122d4b6c3c
Dasse <pascaldasse56@gmail.com>
2025-06-19 10:04:35 +0200

    add blog create + edit  +tnymce

---
+++ config/tinymce.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+	'config' => [
+		'language'       => env('APP_TINYMCE_LOCALE', 'en_US'),
+		'plugins'        => 'fullscreen',
+		'toolbar'        => 'undo redo style | fontfamily fontsize | alignleft aligncenter alignright alignjustify | bullist numlist | copy cut paste pastetext | hr | link image quicktable | fullscreen',
+		'toolbar_sticky' => true,
+		'min_height'     => 50,
+		'license_key'    => 'gpl',
+		'valid_elements' => '*[*]',
+
+	],
+
+	'config_page' => [
+		'language'       => env('APP_TINYMCE_LOCALE', 'en_US'),
+		'plugins'        => 'codesample fullscreen',
+		'toolbar'        => 'undo redo style | fontfamily fontsize | alignleft aligncenter alignright alignjustify | bullist numlist | copy cut paste pastetext | hr | codesample | link image quicktable | fullscreen',
+		'toolbar_sticky' => true,
+		'min_height'     => 1000,
+		'license_key'    => 'gpl',
+		'valid_elements' => '*[*]',
+	],
+    'config_comment' => [
+		'language'       => env('APP_TINYMCE_LOCALE', 'en_US'),
+		'plugins'        => 'codesample',
+		'toolbar'        => 'undo redo | styles | copy cut paste pastetext | hr | codesample',
+		'toolbar_sticky' => true,
+		'min_height'     => 300,
+		'license_key'    => 'gpl',
+	],
+];
